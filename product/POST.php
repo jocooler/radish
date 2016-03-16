e8ceed6dd22174d5b286e741b75436aac8305261
= <=>
2016-03-16 14:12:36 -0400

    Part of product post

--- product/POST.php
+++ product/POST.php
@@ -1,8 +1,30 @@
 <?php
-return $response->write("create a new product");
-protected $post_query_string = array(
-  "INSERT INTO products (`sku`, `upc`, `name`, `manufacturer`, `category`, `wholesale`, `taxable`) VALUES (:sku, :upc, :name, :manufacturer, :category, :wholesale, :taxable)",
+//TODO not yet done
+class Post_Product extends Product {
+  protected $post_query_string = "INSERT INTO products (`sku`, `upc`, `name`, `manufacturer`, `category`, `wholesale`, `taxable`, `qoh`, `retail`, `discount`, `discount_type`) VALUES (:sku, :upc, :name, :manufacturer, :category, :wholesale, :taxable, :qoh, :retail, :discount, :discount_type)";
+  protected $max_query_string = "SELECT MAX as max from products";
+  protected $check_query_string = "SELECT sku FROM products WHERE upc = :upc";
+  public $body;
 
-  "INSERT INTO burlingtonProducts (`sku`, `qoh`, `retail`, `discount`, `discount_type`) VALUES (:sku, :qoh, :retail, :discount, :discount_type)"
-);
+  public function fetch_details() {
+    $this->set($this->body);
+    $post_query_parameters = array (
+      'sku'   => $this->sku,
+      'upc'   => $this->upc,
+      'name'  => $this->name,
+      'manufacturer'  => $this->manufacturer,
+      'category'      => $this->category,
+      'wholesale'     => $this->wholesale,
+      'taxable'       => $this->taxable,
+      'qoh'           => $this->qoh,
+      'retail'        => $this->retail,
+      'discount'      => $this->discount,
+      'discount_type' => $this->discount_type
+    );
+
+  }
+}
+
+$request = new Request();
+
 ?>
